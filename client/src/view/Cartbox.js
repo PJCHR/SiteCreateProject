6ca53189966e9eaac6aeef4f30e9f6f80886e208
PJCHR <phy4534@gmail.com>
2021-08-30 19:28:03 +0900

    20210830_1930-upload

--- client/src/view/Cartbox.js
+++ client/src/view/Cartbox.js
@@ -20,16 +20,16 @@ class Cartbox extends Component {
     }
 
     cartApi = async () => {
-        await axios.get('/authority',this.state)
-		.then(res => this.setState({ authority: res.data }));
-        await axios.post('/mycart',this.state)
-		.then(res => {this.setState({ result: res.data });})
-		.then(this.onVisible);
+      await axios.get('/authority',this.state)
+		  .then(res => this.setState({ authority: res.data }));
+      await axios.post('/mycart',this.state)
+      .then(res => {this.setState({ result: res.data });})
+      .then(this.onVisible);
     }
 
     reLoginCheck = () => {
-        axios.get('/authority',this.state)
-		.then(res => this.setState({status:res.data.status, authority:res.data}));
+      axios.get('/authority', this.state)
+      .then(res => { this.setState({status:res.data.status, authority:res.data}); });
     }
 
     buyAction = async () => {
@@ -59,52 +59,54 @@ class Cartbox extends Component {
        var json = {num:data}
        var result = window.confirm("해당 물품을 장바구니에서 삭제하겠습니까?");
        if(result){
-		   axios.post("/mycartDelete",this.state,json)
-		   .then(res => res)
-		   .then(setTimeout(()=>document.location.href=document.location.href,1000))
+        axios.post("/mycartDelete", json)
+        .then(res => res)
+        .then(setTimeout(()=>document.location.href = document.location.href, 1000))
        }
        else{
-		   alert('삭제를 취소하였습니다.')
+		    alert('삭제를 취소하였습니다.');
        }
     }
 
   render() {
 	var priceSum = 0;
-    return (
+  return (
     <div className={HomeStyle.body_wrap}>
       <TOP ReturnUrl={document.location.href}/>
       <div id="content">
-        <div className="content_wrap">
-			<div className={M.cart_border}>
-				{
-				this.state.authority.length === 0 ?
-					<div>
-						<div className={M.login_fail}> 로그인 후 이용 부탁드립니다. </div>
-						<button className={M.login_button} onClick={this.loginAction}> 로그인</button>
-					</div>
-					: this.state.result.length >= 1 ? 
+        <div className={M.content_wrap}>
+          <div className={M.cart_wrap}>
+            <div className={M.cart_border}>
+              {
+              this.state.authority.length === 0 ?
+                <div>
+                  <div className={M.login_fail}> 로그인 후 이용 부탁드립니다. </div>
+                  <button className={M.login_button} onClick={this.loginAction}> 로그인</button>
+                </div>
+                : this.state.result.length >= 1 ? 
 
-					this.state.result.map((item, index) => {
-						priceSum += item.pdt_price*item.much;
-						return (
-							<div className={M.item_wrap} key={index}>
-							<img className={M.item_image} src={item.imgsource} alt="이미지" /> 
-							<div className={M.item_product}>{item.pdt_name}</div>
-							<div className={M.item_much}>수량 : {item.much}개</div>
-							<div className={M.item_pay}>{this.comma(item.pdt_price*item.much)} </div>
-							<button className={M.item_delete} onClick={()=>this.deleteAction(item.num)}>삭제</button>
-							</div>
-						)
-					}) :
-					<div className={M.login_fail}> 아직 바구니에 담긴 물건이 없습니다. </div> 
-				}
-			</div>
+                this.state.result.map((item, index) => {
+                  priceSum += item.pdt_price*item.much;
+                  return (
+                    <div className={M.item_wrap} key={index}>
+                    <img className={M.item_image} src={item.imgsource} alt="이미지" /> 
+                    <div className={M.item_product}>{item.pdt_name}</div>
+                    <div className={M.item_much}>수량 : {item.much}개</div>
+                    <div className={M.item_pay}>{this.comma(item.pdt_price*item.much)} </div>
+                    <button className={M.item_delete} onClick={()=>this.deleteAction(item.num)}>삭제</button>
+                    </div>
+                  )
+                }) :
+                <div className={M.login_fail}> 아직 바구니에 담긴 물건이 없습니다. </div> 
+              }
+            </div>
 
-			<div className={M.buy_wrap}>
-				<div className={M.buy_text}> 결제 예정금액 </div>
-				<div className={M.buy_sum}>총 {this.comma(priceSum)}</div>
-				<button className={M.buy_button} onClick={this.buyAction}>결제하기</button>
-			</div>
+            <div className={M.buy_wrap}>
+              <div className={M.buy_text}> 결제 예정금액 </div>
+              <div className={M.buy_sum}>총 {this.comma(priceSum)}</div>
+              <button className={M.buy_button} onClick={this.buyAction}>결제하기</button>
+            </div>
+          </div>
         </div>
       </div>
       <BOTTOM/>
