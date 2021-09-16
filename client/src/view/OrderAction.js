e53627146f612646f3bb1014326e503ae9e7c09f
PJCHR <phy4534@gmail.com>
2021-09-16 12:10:46 +0900

    20210916_1220-upload

--- client/src/view/OrderAction.js
+++ client/src/view/OrderAction.js
@@ -6,7 +6,6 @@ import queryString from "query-string";
 import { TOP, BOTTOM } from './Home';
 
 import HomeStyle from '../css/HomeStyle.module.css';
-import HeaderStyle from '../css/HeaderStyle.module.css';
 import OrderActionStyle from '../css/OrderActionStyle.module.css';
 
 class OrderAction extends Component {
@@ -37,16 +36,26 @@ class OrderAction extends Component {
             <div className={HomeStyle.body_wrap} ReturnUrl={document.location.href}>
                 <div id="gnb">
                     <div className={OrderActionStyle.header_wrap}>
-                        <div className={OrderActionStyle.inner_wrap}>
-                            <h1 className={HeaderStyle.logo}> <Link to='/'>home</Link> </h1>
+                        <div className={OrderActionStyle.inner}>
+                            <h1 className={OrderActionStyle.logo}> <Link to='/'>home</Link> </h1>
                         </div>
                     </div>
                 </div>
                 <div id="content">
-                    <div className={OrderActionStyle.content_wrap}>
-                        <div className={OrderActionStyle.order_title_wrap}>
-                            <h1>주문결제</h1>
-                            {/* 3가지 테이블 생성 */}
+                    <div className={OrderActionStyle.order_content}>
+                        <div className={OrderActionStyle.order_header}>
+                            <div className={OrderActionStyle.order_header_inner}>
+                                <div className={OrderActionStyle.order_title}>
+                                    <h1>주문결제</h1>
+                                </div>
+                                <div className={OrderActionStyle.order_step}>
+                                    <ul className={OrderActionStyle.step}>
+                                        <li><i className={OrderActionStyle.number}>01</i> 장바구니</li>
+                                        <li className={OrderActionStyle.second}><i className={OrderActionStyle.number}>02</i> 주문결제<span className={OrderActionStyle.skip}>현재 단계</span></li>
+                                        <li><i className={OrderActionStyle.number}>03</i> 주문완료</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                         <div className={OrderActionStyle.order_info}>
                             <div className={OrderActionStyle.shopping_adress_wrap}>
@@ -55,11 +64,12 @@ class OrderAction extends Component {
                             <div className={OrderActionStyle.order_product_wrap}>
 
                             </div>
-                        </div>
-                        <div className={OrderActionStyle.payment_info}>
-                            <div className={OrderActionStyle.payment_wrap}>
+                            <div className={OrderActionStyle.payment_info}>
+                                <div className={OrderActionStyle.payment_wrap}>
 
+                                </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
