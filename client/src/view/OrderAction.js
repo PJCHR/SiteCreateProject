b202a3c7eabcadaa7ff841e8482d751106b4e548
PJCHR <phy4534@gmail.com>
2021-11-18 18:09:47 +0900

    20211118_1800-test

--- client/src/view/OrderAction.js
+++ client/src/view/OrderAction.js
@@ -15,7 +15,7 @@ class OrderAction extends Component {
     }
     componentDidMount() {
         this.getQueryString();
-        this.checkRadio();
+        // this.checkRadio();
         this.userAddr();
     }
 
@@ -38,10 +38,6 @@ class OrderAction extends Component {
 
     checkRadio = () => {
         // $(document).ready(function(){
-        //     $("#radioCheck").click(function(event){
-        //         event.preventDefault();
-        //     });
-        // });
 
         // 체크값 확인 코드
         // var v = document.querySelector('input[name="radioCheck"]:checked').value;
@@ -49,26 +45,26 @@ class OrderAction extends Component {
         // console.log(v);
         
         if(v === 'basic'){
-            document.getElementById('addr1').style.display = "block"; // 기본배송지
-            // $('r2').prop('checked',false);
-            // $('r1').prop('checked',true);
-        }else{
-            document.getElementById('addr1').style.display = "none"; // 숨김
+            document.getElementById('div_view1').style.display = "none"; // 받는사람 INPUT
+            // document.getElementById('div_view2').style.display = "none"; // 주소 INPUT
+            // document.getElementById('div_view3').style.display = "none"; // 전화번호 INPUT
+
+            document.getElementById('div_01').style.display = "block"; // 받는사람 TEXT
+            document.getElementById('div_02').style.display = "block"; // 주소 TEXT
+            document.getElementById('div_03').style.display = "block"; // 전화번호 TEXT
         }
         if(v === 'self'){
-            document.getElementById('addr2').style.display = "block"; // 직접입력
-            // $('r2').prop('checked',true);
-            // $('r1').prop('checked',false);
-        }else{
-            document.getElementById('addr2').style.display = "none"; // 숨김
-        }
+            document.getElementById('div_view1').style.display = "block"; // 받는사람 INPUT
+            // document.getElementById('div_view2').style.display = "block"; // 주소 INPUT
+            // document.getElementById('div_view3').style.display = "block"; // 전화번호 INPUT
 
-        // var cellPhone = document.getElementById('cellPhone');
-        // cellPhone.onkeyup = function(event){
-        // event = event || window.event;
-        // var _val = this.value.trim();
-        // this.value = this.autoHypenPhone(_val) ;
-        // }
+            document.getElementById('div_01').style.display = "none"; // 받는사람 TEXT
+            document.getElementById('div_02').style.display = "none"; // 주소 TEXT
+            document.getElementById('div_03').style.display = "none"; // 전화번호 TEXT
+        }
+        
+        // $('r2').prop('checked',true);
+        // $('r1').prop('checked',false);
     }
     
     userAddr = async () => {
@@ -77,8 +73,6 @@ class OrderAction extends Component {
         await axios.post('/orderaction',this.state)
         .then(res => this.setState({ userData: res.data }))
         .catch(err => console.log(err));
-
-        
     }
 
     render(){
@@ -122,67 +116,100 @@ class OrderAction extends Component {
                                                     {/* 기본 배송지 */}
                                                     <li>
                                                         {/* <label><input type="radio" name="radiobox" value="rdo1">rdo1</input></label> */}
-                                                        <label><input type="radio" name="radioCheck" id="r1" value="basic" onClick={this.checkRadio} checked/><span>기본배송지</span></label>
+                                                        <label><input type="radio" name="radioCheck" value='basic' onClick={this.checkRadio} defaultChecked/><span>기본배송지</span></label>
                                                     </li>
                                                     {/* 직접 입력 */}
                                                     <li>
-                                                        <label><input type="radio" name="radioCheck" id="r2" value="self" onClick={this.checkRadio} /><span>직접입력</span></label>
+                                                        <label><input type="radio" name="radioCheck" value='self' onClick={this.checkRadio} /><span>직접입력</span></label>
                                                     </li>
                                                 </ul>
                                             </div>
                                             
                                             {/* 배송지 라디오 체크에 따라 레이아웃 바뀜 */}
                                             {/* 기본배송지 */}
-                                            <div className={OrderActionStyle.c_order_delivery} id="addr1" style={{display:"none"}}>
+                                            <div className={OrderActionStyle.c_order_delivery}>
                                                 <h3 className={OrderActionStyle.skip}>기본배송지</h3>
                                                 {userData.map((item,number) => {
                                                 return(
                                                 <div className={OrderActionStyle.c_order_delivery_info}>
                                                     <div className={OrderActionStyle.c_order_address}>
                                                         <dl>
-                                                            {/* 받는 사람 */}
-                                                            <div className={OrderActionStyle.name}>
+                                                            {/* 받는 사람 TEXT */}
+                                                            <div className={OrderActionStyle.name} id="div_01" >
                                                                 <dt className={OrderActionStyle.skip}>받으시는 분</dt>
                                                                 <dd>
-                                                                    <span id="dlv_view_01_html">{item.nickname}</span>
+                                                                    <span>{item.nickname}</span>
                                                                 </dd>
                                                             </div>
-                                                            {/* 주소 */}
-                                                            <div className={OrderActionStyle.address}>
+                                                            {/* 주소 TEXT */}
+                                                            <div className={OrderActionStyle.address} id="div_02">
                                                                 <dt className={OrderActionStyle.skip}>주소</dt>
                                                                 <dd>
                                                                     <div className={OrderActionStyle.form_box}>
-                                                                        <span id="dlv_view_02_html">{item.addr}</span>
+                                                                        <span>{item.addr}</span>
                                                                     </div>
                                                                 </dd>
                                                             </div>
-                                                            {/* 휴대전화 */}
-                                                            <div id="dlv_03_view">
-                                                                <div className={OrderActionStyle.phone_number}>
-                                                                    <dt className={OrderActionStyle.skip}>휴대전화</dt>
-                                                                    <dd><span id="dlv_view_03_html">{item.phone}</span></dd>
-                                                                </div>
+                                                            {/* 휴대전화 TEXT */}
+                                                            <div className={OrderActionStyle.phone_number} id="div_03">
+                                                                <dt className={OrderActionStyle.skip}>휴대전화</dt>
+                                                                <dd><span>{item.phone}</span></dd>
                                                             </div>
+
+                                                            {/* 받는 사람 INPUT */}
+                                                            <div className={OrderActionStyle.field} id="div_view1" style={{display: 'none'}}>
+                                                                <dt className={OrderActionStyle.skip}><label for="rcvrNm">받으시는 분</label></dt>
+                                                                <dd>
+                                                                    {/* 값을 넣는 것에 따라 state에 저장 */}
+                                                                    <input type="text" className={OrderActionStyle.name} id="rcvrNm" name="rcvrNm" maxlength="40" value="" placeholder="받는 사람" title="받는 사람"/>
+                                                                </dd>
+                                                                <input type="hidden" id="rcvrNmTmp" name="rcvrNmTmp" value=""/>
+                                                            </div>
+                                                            
                                                         </dl>
                                                     </div>
                                                 </div>
                                                 )})}
                                             </div>
-
-                                            {/* 직접입력 */}
-                                            <div className={OrderActionStyle.c_order_delivery} id="addr2" style={{display:"none"}}>직접입력
-                                                <div className={OrderActionStyle.c_order_delivery_info}>
-                                                    
-                                                </div>
-                                            </div>
                                             
                                             {/* 요청사항 선택 */}
-                                            <div className={OrderActionStyle.c_order_delivery_request}></div>
+                                            <div className={OrderActionStyle.c_order_delivery}>
+                                                <h3 className={OrderActionStyle.skip}><label for="prdOrdDlvReqCont0">배송시요구사항</label></h3>
+                                                <div className={OrderActionStyle.delivery_info}>
+                                                    <div className={OrderActionStyle.c_order_delivery_request}>
+                                                    
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
 
-                                    <div className={OrderActionStyle.order_goods}>
-                                        구매 상품 게시
+                                    <div className={OrderActionStyle.b_order_goods}>
+                                        <div className={OrderActionStyle.c_order_title}>
+                                            <h2 className={OrderActionStyle.title}>주문상품</h2>
+                                            <p className={OrderActionStyle.text}>상품수량 및 옵션변경은 상품상세 또는 장바구니에서 가능합니다.</p>
+                                        </div>
+                                        <div className={OrderActionStyle.c_order_content}>
+                                            <div className={OrderActionStyle.c_order_store_product}>
+
+                                                <div className={OrderActionStyle.c_order_title}>
+                                                    <h3 className={OrderActionStyle.title}><span className={OrderActionStyle.ico_store}>스토어명</span><span className={OrderActionStyle.store} id="storeNm">스파클생수 </span></h3>
+                                                </div>
+
+                                                <div className={OrderActionStyle.c_order_cart_list}>
+                                                    <ul>
+                                                        <li className={OrderActionStyle.group_prd}>
+                                                            <ul>
+                                                                
+                                                            </ul>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                        
+                                        
                                     </div>
                                 </div>
                                 <div className={OrderActionStyle.order_payment}>
